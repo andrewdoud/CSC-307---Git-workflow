59191714fd85f6cb4ef7a89ff79938a5cfdedf5b
rmevorac <rmevorac@calpoly.edu>
2021-01-07 19:05:17 -0800

    Roey's changes to Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Andrew! You are a third year.");
+		System.out.println("Hello, Roey! You are also a third year.");
 	}
 }
 
